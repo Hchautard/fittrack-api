408f51eeb1cf63b2f0cd31d1bd0796307d5dc053
Hugo <chautardhugo@gmail.com>
2026-04-21 22:00:29 +0200

    Add config (json type) property to User. Add jackson dependency.

--- src/main/java/com/fittrack_api/user/User.java
+++ src/main/java/com/fittrack_api/user/User.java
@@ -1,6 +1,11 @@
 package com.fittrack_api.user;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Date;
+import java.util.Map;
 
 @Entity
 @Table(name = "users")
@@ -13,6 +18,18 @@ public class User {
     public String name;
     private String email;
     private String password;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String zip;
+    private String gender;
+    private Date birthday;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private Map<String, Object> config;
 
     public Integer getId() {
         return id;
@@ -46,4 +63,75 @@ public class User {
         this.password = password;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Map<String, Object> getConfig() {
+        return config;
+    }
+
+    public void setConfig(Map<String, Object> config) {
+        this.config = config;
+    }
 }
--- src/main/java/com/fittrack_api/workout/Workout.java
+++ src/main/java/com/fittrack_api/workout/Workout.java
@@ -13,5 +13,77 @@ public class Workout {
     List<Exercise> exercises;
     Integer pumpScore;
     Integer nerveScore;
-    Integer tensionScore;
+    Integer stressScore;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
+
+    public Integer getPumpScore() {
+        return pumpScore;
+    }
+
+    public void setPumpScore(Integer pumpScore) {
+        this.pumpScore = pumpScore;
+    }
+
+    public Integer getNerveScore() {
+        return nerveScore;
+    }
+
+    public void setNerveScore(Integer nerveScore) {
+        this.nerveScore = nerveScore;
+    }
+
+    public Integer getStressScore() {
+        return stressScore;
+    }
+
+    public void setStressScore(Integer stressScore) {
+        this.stressScore = stressScore;
+    }
 }
