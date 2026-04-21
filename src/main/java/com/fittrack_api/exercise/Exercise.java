c98d105e7353411b457be073cb256fa42c5599b2
Hugo <chautardhugo@gmail.com>
2026-04-21 21:31:51 +0200

    Create muscle and exercise classes

---
+++ src/main/java/com/fittrack_api/exercise/Exercise.java
@@ -0,0 +1,33 @@
+package com.fittrack_api.exercise;
+
+import com.fittrack_api.muscle.Muscle;
+
+public class Exercise {
+    String name;
+    String description;
+    Muscle muscle;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Muscle getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(Muscle muscle) {
+        this.muscle = muscle;
+    }
+}
---
+++ src/main/java/com/fittrack_api/muscle/Muscle.java
@@ -0,0 +1,31 @@
+package com.fittrack_api.muscle;
+
+public class Muscle {
+    String name;
+    String type;
+    String group;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
