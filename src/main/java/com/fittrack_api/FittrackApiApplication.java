bac4136fe700bc4ca5731684b660fc21852bf034
Hugo <chautardhugo@gmail.com>
2026-04-24 10:00:00 +0200

    Refactor la structure du dossier

--- src/main/java/com/fittrack_api/FittrackApiApplication.java
+++ src/main/java/com/fittrack_api/FittrackApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.fittrack_api;
+package com.fittrack_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fittrack_api/FittrackApiApplicationTests.java
+++ src/test/java/com/fittrack_api/FittrackApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.fittrack_api;
+package com.fittrack_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
